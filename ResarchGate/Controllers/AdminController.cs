c9e6dff41abc1899b75b55c245fa067e499c45f7
Marian Nader <73747488+mariannader@users.noreply.github.com>
2022-05-02 00:12:44 +0200

    Add project files.

---
+++ ResarchGate/Controllers/AdminController.cs
@@ -0,0 +1,138 @@
+﻿using ResarchGate.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ResarchGate.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        dbreserchgateEntities db = new dbreserchgateEntities();
+
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(tbl_user uvm, HttpPostedFileBase imgfile)
+        {
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+                tbl_user u = new tbl_user();
+                u.u_name = uvm.u_name;
+                u.u_email = uvm.u_email;
+                u.u_password = uvm.u_password;
+                u.u_image = path;
+                u.u_contact = uvm.u_contact;
+                u.u_position = uvm.u_position;
+                db.tbl_user.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("login");
+                
+
+            }
+
+            return View();
+        } //method......................... end.....................
+
+        
+
+
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult login(tbl_user avm)
+        {
+            tbl_user ad = db.tbl_user.Where(x => x.u_email == avm.u_email && x.u_password == avm.u_password).SingleOrDefault();
+            if (ad != null)
+            {
+
+                Session["u_id"] = ad.u_id.ToString();
+                return RedirectToAction("profile");
+
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+
+            }
+
+            return View();
+        }
+
+
+        public ActionResult profile()
+        {
+            return View();
+        }
+
+
+        public string uploadimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                        //    ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
+
+
+    }
+}
